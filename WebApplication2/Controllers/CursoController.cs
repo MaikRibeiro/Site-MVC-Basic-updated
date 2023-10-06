a1d530e08b60f4c0c06d71dc9f998a851ed9cfea
Michael Ribeiro <michael.douglas2020@hotmail.com>
2023-10-06 12:41:55 -0300

    Add project files.

---
+++ WebApplication2/Controllers/CursoController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    
+
+    public class CursoController : Controller
+    {
+        // GET: Curso
+        public ActionResult Index()
+        {
+            return View(CursoModel.CriarLista());
+        }
+
+        // GET: Curso/Details/5
+        public ActionResult Details(int id)
+        {
+            var curso = CursoModel.CriarLista()[id];
+            return View(curso);
+        }
+
+        // GET: Curso/Create
+        public ActionResult Create()
+        {
+            var curso = CursoModel.CriarLista();
+
+            return View();
+        }
+
+        // POST: Curso/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CursoModel collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Curso/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var curso = CursoModel.CriarLista()[id];
+
+            return View(curso);
+
+        }
+
+        // POST: Curso/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit()
+        {
+
+            return View();
+        }
+
+        // GET: Curso/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var curso = CursoModel.CriarLista()[id];
+
+            //  var curso = CursoModel.CriarLista().Where(o => o.Id == Id).FirstOrDefault();
+            return View(curso);
+        }
+
+        // POST: Curso/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(CursoModel Curso, IFormCollection collection)
+        {
+
+
+            return RedirectToAction("Index");
+        }
+    }
+}
+  
---
+++ WebApplication2/Models/CursoModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class CursoModel
+    {
+        public string Nome { get; set; }
+        public string Modalidade { get; set; }
+        public int Vagas { get; set; }
+
+        public static List<CursoModel> CriarLista()
+        {
+            var lista = new List<CursoModel>();
+
+            lista.Add(new CursoModel() { Nome = "Desenvolvimento de Sistemas", Modalidade = "Presencial", Vagas = 40});
+            lista.Add(new CursoModel() { Nome = "Contabilidade", Modalidade = "Semi-Presencial", Vagas = 40});
+            lista.Add(new CursoModel() { Nome = "Administração", Modalidade = "EAD", Vagas = 40});
+            lista.Add(new CursoModel() { Nome = "Edificações", Modalidade = "Presencial (20% EAD)", Vagas = 80});
+            lista.Add(new CursoModel() { Nome = "Recursos Humanos", Modalidade = "EAD", Vagas = 40});
+
+            return lista;
+        }
+    }
+}
